286ed1475429be420a7528e7444db49d5004a80b
spiritistz <spiritistz@gmail.com>
2015-03-31 15:24:28 +0100

    Update
    Try out some markers

--- HiFilm/src/com/wordpress/hifilmtheapp/MainActivity.java
+++ HiFilm/src/com/wordpress/hifilmtheapp/MainActivity.java
@@ -1,194 +1,94 @@
 package com.wordpress.hifilmtheapp;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.List;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import android.os.AsyncTask;
-import android.app.Dialog;
-import android.location.Location;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentActivity;
-import android.widget.TextView;
-import android.view.Menu;
- 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.maps.CameraUpdateFactory;
+
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
  
 public class MainActivity extends FragmentActivity {
- 
-	GoogleMap mGoogleMap;
-	 
+	
+    static final LatLng LONDON = new LatLng(51.507033, -0.127716);
+	static final LatLng UOW = new LatLng(51.57761, -0.324568);
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
- 
-        // Getting reference to SupportMapFragment
-        SupportMapFragment fragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
- 
-        // Creating GoogleMap from SupportMapFragment
-        mGoogleMap = fragment.getMap();
- 
-        // Enabling MyLocation button for the Google Map
-        mGoogleMap.setMyLocationEnabled(true);
- 
-        // Setting OnClickEvent listener for the GoogleMap
-        mGoogleMap.setOnMapClickListener(new OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latlng) {
-                addMarker(latlng);
-                sendToServer(latlng);
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
             }
-        });
- 
-        // Starting locations retrieve task
-        new RetrieveTask().execute();
+        }
     }
- 
-    // Adding marker on the GoogleMaps
-    private void addMarker(LatLng latlng) {
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(latlng);
-        markerOptions.title(latlng.latitude + "," + latlng.longitude);
-        mGoogleMap.addMarker(markerOptions);
-    }
- 
-    // Invoking background thread to store the touched location in Remove MySQL server
-    private void sendToServer(LatLng latlng) {
-        new SaveTask().execute(latlng);
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+    	
+    	mMap.setMyLocationEnabled(true);
+    	mMap.getUiSettings().setCompassEnabled(true);
+    	mMap.getUiSettings().setRotateGesturesEnabled(true);
+    	
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("HiFilm")
+                .icon(BitmapDescriptorFactory
+                .fromResource(R.drawable.marker)));
+        mMap.addMarker(new MarkerOptions()
+                .position(LONDON)
+                .title("London")
+                .icon(BitmapDescriptorFactory
+                .fromResource(R.drawable.marker)));
+        mMap.addMarker(new MarkerOptions()
+                .position(UOW)
+                .title("University of Westminster")
+                .snippet("Check out films here!")
+                .icon(BitmapDescriptorFactory
+                .fromResource(R.drawable.marker)));                
     }
     
-    // Background thread to save the location in remove MySQL server
-    private class SaveTask extends AsyncTask<LatLng, Void, Void> {
-        @Override
-        protected Void doInBackground(LatLng... params) {
-            String lat = Double.toString(params[0].latitude);
-            String lng = Double.toString(params[0].longitude);
-            String strUrl = "http://192.168.1.3/location_marker_mysql/save.php";
-            URL url = null;
-            try {
-                url = new URL(strUrl);
- 
-                HttpURLConnection connection = (HttpURLConnection) url
-                    .openConnection();
-                connection.setRequestMethod("POST");
-                connection.setDoOutput(true);
-                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(
-                connection.getOutputStream());
- 
-                outputStreamWriter.write("lat=" + lat + "&lng="+lng);
-                outputStreamWriter.flush();
-                outputStreamWriter.close();
- 
-                InputStream iStream = connection.getInputStream();
-                BufferedReader reader = new BufferedReader(new
-                InputStreamReader(iStream));
- 
-                StringBuffer sb = new StringBuffer();
- 
-                String line = "";
- 
-                while( (line = reader.readLine()) != null){
-                    sb.append(line);
-                }
- 
-                reader.close();
-                iStream.close();
- 
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
- 
-            return null;
-        }
-    }
- 
-    // Background task to retrieve locations from remote mysql server
-    private class RetrieveTask extends AsyncTask<Void, Void, String>{
- 
-        @Override
-        protected String doInBackground(Void... params) {
-            String strUrl = "http://192.168.1.3/location_marker_mysql/retrieve.php";
-            URL url = null;
-            StringBuffer sb = new StringBuffer();
-            try {
-                url = new URL(strUrl);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
-                InputStream iStream = connection.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(iStream));
-                String line = "";
-                while( (line = reader.readLine()) != null){
-                    sb.append(line);
-                }
- 
-                reader.close();
-                iStream.close();
- 
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return sb.toString();
-        }
- 
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            new ParserTask().execute(result);
-        }
-    }
- 
-    // Background thread to parse the JSON data retrieved from MySQL server
-    private class ParserTask extends AsyncTask<String, Void, List<HashMap<String, String>>>{
-        @Override
-        protected List<HashMap<String,String>> doInBackground(String... params) {
-            MarkerJSONParser markerParser = new MarkerJSONParser();
-            JSONObject json = null;
-            try {
-                json = new JSONObject(params[0]);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            List<HashMap<String, String>> markersList = markerParser.parse(json);
-            return markersList;
-        }
- 
-        @Override
-        protected void onPostExecute(List<HashMap<String, String>> result) {
-            for(int i=0; i<result.size();i++){
-                HashMap<String, String> marker = result.get(i);
-                LatLng latlng = new LatLng(Double.parseDouble(marker.get("lat")), Double.parseDouble(marker.get("lng")));
-                addMarker(latlng);
-            }
-        }
-    }
- 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 }
